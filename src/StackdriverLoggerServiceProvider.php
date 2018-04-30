c6575fead8f63acd4aa853b7f078536a1ab74c26
Bas <basvvliet@gmail.com>
2018-04-30 14:11:14 +0200

    use laravel 5.6 logging interface. Make provider usable for both Laravel and Lumen

--- src/StackdriverLoggerServiceProvider.php
+++ src/StackdriverLoggerServiceProvider.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 namespace Wonderkind\StackdriverLogging;
 
 use Google\Cloud\Logging\LoggingClient;
-use Illuminate\Contracts\Events\Dispatcher;
-use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Log\Writer;
 use Illuminate\Support\ServiceProvider;
 use Wonderkind\StackdriverLogging\Handler\StackdriverLoggingHandler;
 
@@ -18,7 +15,7 @@ class StackdriverLoggerServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->publishes([
-            __DIR__ . '/config/stackdriver.php' => config_path('stackdriver.php'),
+            __DIR__ . '/config/stackdriver.php' => $this->configFilePath(),
         ]);
     }
 
@@ -27,23 +24,51 @@ class StackdriverLoggerServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        $this->mergeStackdriverConfig();
+        $this->bindLoggingClient();
+        $this->bindLogHandler();
+    }
+
+    /**
+     *
+     */
+    protected function mergeStackdriverConfig(): void
+    {
         $this->mergeConfigFrom(__DIR__ . '/config/stackdriver.php', 'stackdriver');
+    }
+
+    /**
+     *
+     */
+    protected function bindLoggingClient(): void
+    {
         $this->app->bind(LoggingClient::class, function () {
             return new LoggingClient(['projectId' => config('stackdriver.project_id')]);
         });
+    }
 
-        $this->app->bind(StackdriverLoggingHandler::class, function (Application $app) {
+    /**
+     *
+     */
+    protected function bindLogHandler(): void
+    {
+        $this->app->bind(StackdriverLoggingHandler::class, function ($app) {
             /** @var LoggingClient $loggingClient */
             $loggingClient = $app->make(LoggingClient::class);
 
             return new StackdriverLoggingHandler($loggingClient->psrLogger(config('stackdriver.log')));
         });
+    }
 
-        $this->app->bind(Logger::class, function (Application $app) {
-            return new Writer(
-                new \Monolog\Logger('stackdriver', [$app->make(StackdriverLoggingHandler::class)]),
-                $app->make(Dispatcher::class)
-            );
-        });
+    /**
+     * @return string
+     */
+    private function configFilePath(): string
+    {
+        if (function_exists('config_path')) {
+            return config_path('stackdriver.php');
+        }
+
+        return $this->app->basePath() . '/config/stackdriver.php';
     }
 }
