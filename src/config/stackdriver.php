e659ba19473b7d206b842ee80d2d0fb8c31a69f3
Bas <basvvliet@gmail.com>
2018-05-16 16:10:17 +0200

    Added a description and reference to the project to the configuration file

--- src/config/stackdriver.php
+++ src/config/stackdriver.php
@@ -1,6 +1,22 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Google Stackdriver logging
+|--------------------------------------------------------------------------
+|
+| A log channel that can be used as a Monolog logger to log to GCP Stackdriver
+| More information: https://github.com/wonderkind/stackdriver-logging
+*/
+
 return [
+    /*
+     * Your Google Cloud Platform project ID
+     */
     'project_id' => env('GOOGLE_STACKDRIVER_PROJECT_ID'),
+
+    /*
+     * The name of the log to write to
+     */
     'log'        => env('GOOGLE_STACKDRIVER_LOG'),
 ];
