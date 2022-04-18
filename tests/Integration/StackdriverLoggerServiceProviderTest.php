03a92b028d7520c13e81002622eabf3884f5f606
dyriavin <alexander.dyriavin@wonderkind.com>
2022-04-18 19:09:26 +0200

    [WL-3105] chore: PSR 12 syntax

--- tests/Integration/StackdriverLoggerServiceProviderTest.php
+++ tests/Integration/StackdriverLoggerServiceProviderTest.php
@@ -1,5 +1,8 @@
 <?php
+
 declare(strict_types=1);
+
+namespace Wonderkind\StackdriverLogging\Test\Integration;
 
 use Illuminate\Log\Logger;
 use Illuminate\Support\Facades\Log;
@@ -9,11 +12,10 @@ use Wonderkind\StackdriverLogging\LoggerInterface;
 use Wonderkind\StackdriverLogging\StackdriverLogger;
 use Wonderkind\StackdriverLogging\StackdriverLoggerServiceProvider;
 
-class StackdriverLoggerServiceProviderTest extends TestCase
+final class StackdriverLoggerServiceProviderTest extends TestCase
 {
     /**
      * @param \Illuminate\Foundation\Application $app
-     * @return array
      */
     protected function getPackageProviders($app): array
     {
@@ -23,7 +25,7 @@ class StackdriverLoggerServiceProviderTest extends TestCase
     /**
      * @test
      */
-    public function it_loads_the_configuration()
+    public function it_loads_the_configuration(): void
     {
         $projectId = env('GOOGLE_STACKDRIVER_PROJECT_ID');
         $log = env('GOOGLE_STACKDRIVER_LOG');
@@ -36,17 +38,17 @@ class StackdriverLoggerServiceProviderTest extends TestCase
     /**
      * @test
      */
-    public function it_uses_the_same_config_path_if_function_does_not_exists()
+    public function it_uses_the_same_config_path_if_function_does_not_exists(): void
     {
         // Used when using Lumen. which does not have the config_path helper.
         // It is tested this way so we don't have to override the function_exists function
         $this->assertEquals(config_path('stackdriver.php'), $this->app->basePath() . '/config/stackdriver.php');
     }
-    
+
     /**
      * @test
      */
-    public function it_binds_the_log_handler()
+    public function it_binds_the_log_handler(): void
     {
         $handler = $this->app->make(StackdriverLoggingHandler::class);
         $this->assertInstanceOf(StackdriverLoggingHandler::class, $handler);
@@ -55,27 +57,32 @@ class StackdriverLoggerServiceProviderTest extends TestCase
     /**
      * @test
      */
-    public function it_can_be_injected_from_the_container()
+    public function it_can_be_injected_from_the_container(): void
     {
         $logger = $this->app->make(LoggerInterface::class);
         $this->assertInstanceOf(Logger::class, $logger);
         $this->assertInstanceOf(StackdriverLogger::class, $logger);
-        $this->assertInstanceOf(
-            StackdriverLoggingHandler::class,
-            array_first($logger->getLogger()->getHandlers())
-        );
+
+        $haystack = $logger->getLogger()->getHandlers();
+        $handler = $haystack[array_key_first($haystack)];
+
+        $this->assertInstanceOf(StackdriverLoggingHandler::class, $handler);
     }
 
     /**
      * @test
      */
-    public function it_can_be_registered_as_log_channel()
+    public function it_can_be_registered_as_log_channel(): void
     {
         config(['logging.channels.stackdriver' => [
             'driver' => 'monolog',
-            'handler' => Wonderkind\StackdriverLogging\Handler\StackdriverLoggingHandler::class
+            'handler' => StackdriverLoggingHandler::class
         ]]);
-        $handler = array_first(Log::channel('stackdriver')->getLogger()->getHandlers());
+
+
+        $haystack = Log::channel('stackdriver')->getLogger()->getHandlers();
+        $handler = $haystack[array_key_first($haystack)];
+
         $this->assertInstanceOf(StackdriverLoggingHandler::class, $handler);
     }
 }
